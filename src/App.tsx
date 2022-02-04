214b12794b32a64b65f1b139c5cadff317a8afe6
malewis5 <mlewis@peakactivity.com>
2022-02-04 12:53:46 -0500

    moved to context api

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
   PayPalButtonColor,
   PayPalButtonLabel,
   PayPalButtonShape,
+  GatewayProvider,
 } from "@peakactivity/revcom-payment-components";
 import "@peakactivity/revcom-payment-components/lib/index.css";
 import {
@@ -22,69 +23,69 @@ import { applePaymentError, applePaymentSuccess } from "./utils/paymentSuccess";
 
 function App() {
   const [amount, setAmount] = React.useState<string>("1");
-  const { clientInstance, serverError, isLoading } = useGenerateClientToken();
+  const { serverError, isLoading } = useGenerateClientToken();
   const isSupported = IsApplePaySupported();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="dollar-input">
-          <p>Amount must be greater than $0</p>
-          <input
-            type="number"
-            pattern="[0-9]*"
-            min="2"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </div>
+    <GatewayProvider>
+      <div className="App">
+        <header className="App-header">
+          <div className="dollar-input">
+            <p>Amount must be greater than $0</p>
+            <input
+              type="number"
+              pattern="[0-9]*"
+              min="2"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
 
-        <div className="button-container">
-          {isLoading && <p>Loading...</p>}
-          {!isLoading && (
-            <>
-              {isSupported && (
-                <ApplePayButton
-                  onPaymentSuccess={applePaymentSuccess}
-                  onPaymentError={applePaymentError}
-                  payment={{
-                    subtotal: amount,
-                    shipping: "0",
-                    tax: "0",
+          <div className="button-container">
+            {isLoading && <p>Loading...</p>}
+            {!isLoading && (
+              <>
+                {isSupported && (
+                  <ApplePayButton
+                    onPaymentSuccess={applePaymentSuccess}
+                    onPaymentError={applePaymentError}
+                    payment={{
+                      subtotal: amount,
+                      shipping: "0",
+                      tax: "0",
+                    }}
+                    storeName="Sandbox Demo"
+                    shippingMethods={appleStaticShipping}
+                    shippingHandler={appleShipping}
+                    taxHandler={appleTax}
+                    buttonType={ApplePayButtonLabel.buy}
+                    buttonColor={ApplePayColorLabel.white}
+                  />
+                )}
+
+                <PayPalButton
+                  amount={amount}
+                  style={{
+                    color: PayPalButtonColor.blue,
+                    label: PayPalButtonLabel.pay,
+                    shape: PayPalButtonShape.rect,
+                    height: 30,
                   }}
-                  storeName="Sandbox Demo"
-                  client={clientInstance}
-                  shippingMethods={appleStaticShipping}
-                  shippingHandler={appleShipping}
-                  taxHandler={appleTax}
-                  buttonType={ApplePayButtonLabel.buy}
-                  buttonColor={ApplePayColorLabel.white}
+                  shippingHandler={payPalShipping}
+                  taxHandler={payPalTax}
+                  onPaymentSuccess={(data: any) => console.log(data)}
+                  onPaymentError={(err: any) => console.log(err)}
                 />
-              )}
-
-              <PayPalButton
-                client={clientInstance}
-                amount={amount}
-                style={{
-                  color: PayPalButtonColor.blue,
-                  label: PayPalButtonLabel.pay,
-                  shape: PayPalButtonShape.rect,
-                  height: 30,
-                }}
-                shippingHandler={payPalShipping}
-                taxHandler={payPalTax}
-                onPaymentSuccess={(data: any) => console.log(data)}
-                onPaymentError={(err: any) => console.log(err)}
-              />
-              <button className="checkout-button">Submit Order</button>
-            </>
+                <button className="checkout-button">Submit Order</button>
+              </>
+            )}
+          </div>
+          {serverError && (
+            <p className="error">Error connecting to payment microservice</p>
           )}
-        </div>
-        {serverError && (
-          <p className="error">Error connecting to payment microservice</p>
-        )}
-      </header>
-    </div>
+        </header>
+      </div>
+    </GatewayProvider>
   );
 }
 
